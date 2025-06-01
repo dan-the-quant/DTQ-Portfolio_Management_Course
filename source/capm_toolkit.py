617ae689bcc0e61c7ea234183aedd6d459c299ef
Edgar <edgarallo0@hotmail.com>
2025-06-01 16:26:59 -0600

    Module 5 Changes

--- source/capm_toolkit.py
+++ source/capm_toolkit.py
@@ -11,64 +11,6 @@ def wexp(N, half_life):
     return np.flip(w / np.sum(w))
 
 
-# Weighted Moving Averages
-def weighted_moving_average(data, window_size):
-    # Exponential Weights
-    weights = window_size * wexp(window_size, window_size / 2)
-
-    return data.rolling(window=window_size).apply(lambda x: np.dot(x, weights) / weights.sum(), raw=True)
-
-
-# Helper: exclude tiny returns
-def n_days_nonmiss(
-        returns,
-        tiny_ret=1e-6
-):
-    ix_ret_tiny = np.abs(returns) <= tiny_ret
-    return np.sum(~ix_ret_tiny, axis=0)
-
-
-# Relative Strength Calculation
-def calc_rstr(
-        returns,
-        half_life=126,
-        min_obs=100,
-):
-    rstr = np.log(1. + returns)
-    if half_life == 0:
-        weights = np.ones_like(rstr)
-    else:
-        weights = len(returns) * np.asmatrix(wexp(len(returns), half_life)).T
-    rstr = np.sum(rstr * weights)
-    idx = n_days_nonmiss(returns) < min_obs
-    rstr.where(~idx, other=np.nan, inplace=True)
-    df = pd.Series(rstr)
-    df.name = returns.index[-1]
-    return df
-
-
-# Rolling Relative Strength
-def rolling_calc_rstr(
-        returns,
-        window_size=252,
-        half_life=126,
-        min_obs=100
-):
-    rolling_results = []
-    range_to_iter = range(len(returns) - window_size + 1)
-    for i in range_to_iter:
-        window_returns = returns.iloc[i:i + window_size]
-        rs_i = calc_rstr(
-            returns=window_returns,
-            half_life=half_life,
-            min_obs=min_obs
-        )
-
-        rolling_results.append(rs_i)
-
-    return pd.concat(rolling_results, axis=1)
-
-
 # Compute Daily Interest Rate
 def annual_to_daily_rate(
         series,
